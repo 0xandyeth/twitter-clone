ff140d90c7ff895410c94b4041c52b6618e9310a
ReactTop <jjg02016891@gmail.com>
2020-08-20 11:34:34 -0700

    feed_title

--- src/components/feed/components/tweet/Tweet.js
+++ src/components/feed/components/tweet/Tweet.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import Avatar from '@material-ui/core/Avatar';
+import TextField from '@material-ui/core/TextField';
+import {Button} from '@material-ui/core'
 import './tweet.css'
 const Tweet = () => {
   return (
     <div className="tweet">
+        <div className="tweet_title">
+           <Avatar>Z</Avatar>
+            <input
+             placeholder="What's happening?"
+            />
+        </div>
+         <div className="tweet_btn_container">
+           <Button className="tweet_btn" variant="outlined" fullWidth>Tweet</Button>
+         </div>
+        
       
+    
     </div>
   )
 }
